14522cfa7b95e335a086e576f45fecd93eb337ea
Halle Gordon-Jeary <hallegj@googlemail.com>
2023-02-10 20:19:48 +0000

    png is rendered and blue square moves

--- evil.js
+++ evil.js
@@ -1,28 +1,103 @@
-// Called initially and whenever the window resizes to update the canvas
-// size and width/height variables.
-function sizeCanvas() {
-  const canvas = document.getElementById("evil");
-  width = window.innerWidth;
-  height = window.innerHeight;
-  canvas.width = width;
-  canvas.height = height;
-}
-
-// clear canvas and redraw all elements in current position
-const ctx = document.getElementById("evil").getContext("2d");
-  ctx.clearRect(0, 0, width, height);
-//   for (let boid of boids) {
-//     drawBoid(ctx, boid);
-//   }
-
-window.onload = () => {
-    // Make sure the canvas always fills the whole window
-    window.addEventListener("resize", sizeCanvas, false);
-    sizeCanvas();
+(function() {
+    var requestAnimationFrame = window.requestAnimationFrame || window.mozRequestAnimationFrame ||
+      window.webkitRequestAnimationFrame || window.msRequestAnimationFrame;
+    window.requestAnimationFrame = requestAnimationFrame;
+  })();
   
-    // init game
+  //event listener
+  window.addEventListener("keydown", onKeyDown, false);
+  window.addEventListener("keyup", onKeyUp, false);
   
-    // Schedule the main animation loop
+  function onKeyDown(event) {
+    var keyCode = event.keyCode;
+    switch (keyCode) {
+      case 68: //d
+        keyD = true;
+        break;
+      case 83: //s
+        keyS = true;
+        break;
+      case 65: //a
+        keyA = true;
+        break;
+      case 87: //w
+        keyW = true;
+        break;
+    }
+  }
+  
+  function onKeyUp(event) {
+    var keyCode = event.keyCode;
+  
+    switch (keyCode) {
+      case 68: //d
+        keyD = false;
+        break;
+      case 83: //s
+        keyS = false;
+        break;
+      case 65: //a
+        keyA = false;
+        break;
+      case 87: //w
+        keyW = false;
+        break;
+    }
+  }
+  
+  //neccessary variables
+  var tickX = 50;
+  var tickY = 50;
+  
+  var keyW = false;
+  var keyA = false;
+  var keyS = false;
+  var keyD = false;
 
-  };
+  // add temp cake
+  function cakeTest() {
+    window.requestAnimationFrame(cakeTest);
+
+    let newImage = new Image();
+    newImage.src = "./assets/cake1small.png"
+    
+    var canvas = document.getElementById("evilCanvas");
+    var c = canvas.getContext("2d");
+    
+    c.drawImage(newImage, 0, 0, 128, 128);
+
+  }
+  
+  //main animation function
+  function drawStuff() {
+    window.requestAnimationFrame(drawStuff);
+    var canvas = document.getElementById("evilCanvas");
+    var c = canvas.getContext("2d");
+  
+    c.clearRect(0, 0, 1280, 720);
+    c.fillStyle = "blue";
+    c.fillRect(tickX, tickY, 100, 100);
+  
+    if (keyD == true) {
+      tickX += 10;
+    }
+    if (keyS == true) {
+      tickY += 10;
+    }
+    if (keyA == true) {
+      tickX -= 10;
+    }
+    if (keyW == true) {
+      tickY -= 10;
+    }
+  }
+
+//   window.requestAnimationFrame(drawStuff);
+
+  window.onload = () => {
+    drawStuff()
+    cakeTest()
+
+    
+  }
   

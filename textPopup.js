17214bca521c8c13268daf73933787284873dd6d
crackymacky2012 <crackymacky2012@gmail.com>
2023-02-11 15:43:01 +0000

    Added text colour and shadows to text pop up

--- textPopup.js
+++ textPopup.js
@@ -4,7 +4,19 @@ const text = [
 ]
 
 function renderText(c, text) {
-  c.fillText(text, 100, 100);
+  c.font = "bold 36px sans-serif";
+  c.shadowColor = "rgb(190, 190, 190)";
+  c.shadowOffsetX = 10;
+  c.shadowOffsetY = 10;
+  c.shadowBlur = 10;
+  var gradient = c.createLinearGradient(600, 350, 650, 450);
+  gradient.addColorStop(0, "rgb(255, 0, 0)");
+  gradient.addColorStop(1, "rgb(255, 255, 0)");
+  c.fillStyle = gradient;
+  //c.fillStyle = "rgb(255, 0, 0)";
+  c.fillText(text, 650, 350);
+  //c.font = "48px serif"
+  //c.strokeText(text, 650, 350);
 }
 
 function renderCake(c){

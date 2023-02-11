3fb41a841c1a5b7c03827b3887c4f3ba35823a41
JamesBell15 <73955873+JamesBell15@users.noreply.github.com>
2023-02-11 12:30:40 +0000

    Add render text method

--- textPopup.js
+++ textPopup.js
@@ -1,16 +1,15 @@
 const text = [
 
-
-]
-
-function renderText(c, text) {
-  c.fillText(text, 100, 100);
-}
-
 function renderCake(c){
   let cakeSprite = new Image();
   cakeSprite.src = "./assets/cake1.png"
 
   c.drawImage(cakeSprite, 640, 360, 128, 128);
 
+function renderText() {
+  window.requestAnimationFrame(renderText);
+  var canvas = document.getElementById("evilCanvas");
+  var c = canvas.getContext("2d");
+
+  c.fillText("Hello world", 100, 100);
 }

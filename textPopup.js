69c8bc330a107b33804fa45a57d4af5ef2d42871
JamesBell15 <73955873+JamesBell15@users.noreply.github.com>
2023-02-11 17:07:00 +0000

    Remove dup

--- textPopup.js
+++ textPopup.js
@@ -1,27 +1,5 @@
 const text = [
-
-
 ]
-
-function renderText(c, text) {
-  c.font = "bold 36px sans-serif";
-  c.shadowColor = "rgb(190, 190, 190)";
-  c.shadowOffsetX = 10;
-  c.shadowOffsetY = 10;
-  c.shadowBlur = 10;
-  var gradient = c.createLinearGradient(600, 350, 650, 450);
-  gradient.addColorStop(0, "rgb(255, 0, 0)");
-  gradient.addColorStop(1, "rgb(255, 255, 0)");
-  c.fillStyle = gradient;
-  c.fillText(text, 650, 350);
-}
-
-function renderCake(c){
-  let cakeSprite = new Image();
-  cakeSprite.src = "./assets/cake1.png"
-
-  c.drawImage(cakeSprite, 640, 360, 128, 128);
-
 function renderText(c, text) {
   c.font = "bold 36px sans-serif";
   c.shadowColor = "rgb(190, 190, 190)";

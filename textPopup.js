bd7dec1db6d1934f906b9cd2c4f40a754287cd0c
crackymacky2012 <crackymacky2012@gmail.com>
2023-02-11 13:37:35 +0000

    Added cake as object to pick up

--- textPopup.js
+++ textPopup.js
@@ -9,3 +9,11 @@ function renderCake(c){
 function renderText(c, text) {
   c.fillText(text, 100, 100);
 }
+
+function renderCake(c){
+  let cakeSprite = new Image();
+  cakeSprite.src = "./assets/cake1.png"
+
+  c.drawImage(cakeSprite, 640, 360, 128, 128);
+
+}

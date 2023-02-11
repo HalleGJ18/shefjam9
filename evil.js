dd48887671f4a5241cc826342682e51f39138bb1
Halle Gordon-Jeary <hallegj@googlemail.com>
2023-02-11 18:40:37 +0000

    file separation

--- evil.js
+++ evil.js
@@ -4,7 +4,7 @@
     window.requestAnimationFrame = requestAnimationFrame;
   })();
   
-  //event listener
+  //event listeners
   window.addEventListener("keydown", onKeyDown, false);
   window.addEventListener("keyup", onKeyUp, false);
   
@@ -44,102 +44,6 @@
         break;
     }
   }
-
-  // tables 
-  function genTables(c){
-    let level1 = [[30,30,tableSideLen,tableSideLen],
-                  [130,30,tableSideLen,tableSideLen],
-                  [300,490,tableSideLen,tableSideLen],
-                  [870,450,tableSideLen,tableSideLen],
-                  [300,190,tableSideLen,tableSideLen],
-                  [1122,275,tableSideLen,tableSideLen],
-                  [700,350,tableSideLen,tableSideLen],
-                  [30,360,tableSideLen,tableSideLen],
-                  [700,30,tableSideLen,tableSideLen]]
-
-    // level selection
-    tables = level1;
-
-    for (let i = 0; i < tables.length; i++) {
-      tableImgs[i] = new Image();
-      tableImgs[i].src = "./assets/table.png"
-      c.drawImage(tableImgs[i],tables[i][0],tables[i][1],tables[i][2], tables[i][3])
-    }
-  }
-
-  function checkTableCollisions(playerX, playerY, playerSide){
-    let hit = false;
-    for (let t in tables) {
-      hit = overLap(tables[t][0], tables[t][1], playerX, playerY, tableSideLen, playerSide);
-      if (hit){break;}
-      
-    }
-    return hit;
-  }
-
-
-  // cakes
-  
-
-
-  // cart
-
-  function drawCart(c){
-    var cartPos = [640-150, 720-150];
-    var cartImgSide = 300;
-    let cartImg = new Image();
-    cartImg.src = "./assets/cart.png";
-    c.drawImage(cartImg,cartPos[0],cartPos[1],cartImgSide,cartImgSide);
-  }
-
-
-  // player character movement
-  let playerSprite = new Image();
-  playerSprite.src = "./assets/rexW.png";
-
-  var lastImg = "./assets/rexW.png";
-
-  var globalTime = 0;
-  var textTimeOuts = [];
-
-  var cakeSize = tableSideLen/2;
-
-  function movingSprite(c) {
-    
-    var imgWidth = 100;
-    var imgHeight = 100;
-
-    if (keyD == true) {
-      playerSprite.src = "./assets/rexD.png";
-      lastImg = "./assets/rexD.png";
-        if ((offsetX + stepSize + imgWidth <= canvasWidth) && (!(checkTableCollisions(offsetX+stepSize, offsetY, imgWidth)))) {
-            offsetX += stepSize;
-        }
-      }
-    if (keyS == true) {
-      playerSprite.src = "./assets/rexS.png";
-      lastImg = "./assets/rexS.png";
-        if ((offsetY + stepSize + imgHeight <= canvasHeight) && (!(checkTableCollisions(offsetX, offsetY+stepSize, imgHeight)))) {
-            offsetY += stepSize;
-        }
-    }
-    if (keyA == true) {
-      playerSprite.src = "./assets/rexA.png";
-      lastImg = "./assets/rexA.png";
-        if ((offsetX - stepSize >= 0) && (!(checkTableCollisions(offsetX-stepSize, offsetY, imgWidth)))) {
-            offsetX -= stepSize;
-        }
-    }
-    if (keyW == true) {
-      playerSprite.src = "./assets/rexW.png";
-      lastImg = "./assets/rexW.png";
-        if ((offsetY - stepSize >= 0) && (!(checkTableCollisions(offsetX, offsetY-stepSize, imgHeight)))) {
-            offsetY -= stepSize;
-        }
-    }
-    c.drawImage(playerSprite, offsetX, offsetY, imgWidth, imgHeight);
-  }
-
 
   // animation loop
   function animationLoop(){

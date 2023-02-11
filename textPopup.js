063e3c0eefc29140fbb1cab65b81910751760f94
crackymacky2012 <crackymacky2012@gmail.com>
2023-02-11 17:29:57 +0000

    adjusted cake

--- textPopup.js
+++ textPopup.js
@@ -1,18 +1,18 @@
 const text = [
 ]
 function renderText(c, text) {
-  c.font = "bold 36px sans-serif";
+  c.font = "bold 24px sans-serif";
   var gradient = c.createLinearGradient(600, 350, 650, 450);
   gradient.addColorStop(0, "rgb(255, 0, 0)");
   gradient.addColorStop(1, "rgb(255, 255, 0)");
   c.fillStyle = gradient;
-  c.fillText(text, 650, 350);
+  c.fillText(text, 700, 340);
 }
 
 function renderCake(c){
   let cakeSprite = new Image();
   cakeSprite.src = "./assets/cake1.png"
 
-  c.drawImage(cakeSprite, 640, 360, 128, 128);
+  c.drawImage(cakeSprite, 700, 350, 50, 50);
 
 }

687ba126d3866b976d56283b64cb9a8032842fc7
Halle Gordon-Jeary <hallegj@googlemail.com>
2023-02-12 02:18:46 +0000

    level 1 and 2 created

--- constants.js
+++ constants.js
@@ -14,11 +14,6 @@ var tables = [];
 var tableImgs = []
 const tableSideLen = 100;
 
-// cart variables
-var cartImgSide = 300;
-var cartPosX = 640-(cartImgSide/2);
-var cartPosY = 720-(cartImgSide/2);
-
 //player size
 var playerWidth = 100;
 
@@ -42,6 +37,16 @@ var cakes = {};
 var num_of_cakes = 10;
 var cakeImgs = {};
 const cakeOptions = ["./assets/cake1.png", "./assets/cake2.png", "./assets/cake3.png", "./assets/alen.png"];
+
+// cart variables
+var cartImgSide = 300;
+var cartPosX = 640-(cartImgSide/2);
+var cartPosY = 720-(cartImgSide/2);
+
+// player movement vaiables
+var offsetX = cartPosX+100;
+var offsetY = cartPosY+10;
+const stepSize = 7;
 
 // time variables
 var globalTime = 0;
